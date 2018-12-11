4a26e92d4e220bc0f27d96616c7b2ad4c9d8f828
Romain Baguet <romain.baguet@epita.fr>
2018-12-11 07:15:48 -0800

    [MAIN] options handling is done

--- src/main.c
+++ src/main.c
@@ -1,8 +1,63 @@
-# include "includes.h"
+#include "includes.h"
 
-
-int main(int argc, char *argvp[])
+struct options fill_options(int argc, char **argv, struct option *options)
 {
+    static struct options opt =
+    {
+        0
+    };
+    int argv_index = 1;
+    int option_index = 0;
+    char c = ' ';
+    while ((c = getopt_long(argc, argv, "p:m:c:vd", options, &option_index)) != -1)
+    {
+        if (c == '?')
+        {
+            //option not recognized, should throw an error ?
+            break;
+        }
+        switch (c)
+            {
+            case 'p':
+                opt.p = 1;
+                argv_index++;
+                opt.argv_p = argv[argv_index];
+                break;
+            default:
+                break;
+            }
+    }
+    return opt;
+}
 
+struct options get_options(int argc, char **argv)
+{
+    //init options arguments
+    static struct option options[] =
+        {
+            {"pretty-print-torrent-file", 1, NULL, 'p'},
+            {"mktorrent", 1, NULL, 'm'},
+            {"check-integrity", 1, NULL, 'c'},
+            {"dump-peers", 0, NULL, 'd'},
+            {"verbose", 0, NULL, 'v'},
+        };
+    //filling options struct with corresponding values
+    struct options opt = fill_options(argc, argv, options);
+    return opt;
+}
+
+//warnx("my-bittorrent: Usage: ./my-bittorrent [options] [files]"")
+int main(int argc, char *argv[])
+{
+    struct options options = get_options(argc, argv);
+    if (options.p)
+    {
+        printf("ok\n");
+        printf("%s\n", options.argv_p);
+    }
+    else
+    {
+        printf("KO\n");
+    }
     return 0;
 }
