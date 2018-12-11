e8b8fea48a8c640b3d0822ca7c1f303f1288742a
Rayane Amrouche <rayane.amrouche@epita.fr>
2018-12-11 08:18:39 -0800

    [OPTIONS] options structures moved into includes.h

--- src/includes.h
+++ src/includes.h
@@ -1,1 +1,19 @@
+#ifndef INCLUDE_H
+# define INCLUDE_H
+
+# include <err.h>
+# include <getopt.h>
+# include <string.h>
 # include "struct.h"
+
+struct options
+{
+    int nb_options;
+    int nb_argv;
+    char type;
+    char *argv;
+    int d;
+    int v;
+};
+
+#endif /* !INCLUDE_H */
