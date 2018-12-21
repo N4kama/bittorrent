532733d60a70b15d6d1911bd8040b98807877768
Rayane Amrouche <rayane.amrouche@epita.fr>
2018-12-21 06:43:01 -0800

    [CONNECT] can potentially dump but need to get info_hash

--- src/connect/connect.c
+++ src/connect/connect.c
@@ -1,8 +1,38 @@
 #include "connect.h"
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <curl/curl.h>
 
 int dump_peers(const char *path)
 {
-    printf("%s\n", path);
-    return 1;
+    int res = 0;
+    char *n_path = calloc(4096, 1);
+    n_path = strcpy(n_path, path);
+    char *info_hash = "sha1(bencode(metadata['info']))";
+    n_path = strcat(n_path, "?info_hash=");
+    n_path = strcat(n_path, info_hash);
+    n_path = strcat(n_path,
+                    "&peer_id=%2D%41%5A%35%37%35%30%2D%54%70%6B%58%74%74\
+                    %5A%4C%66%70%53%48&port=6881&uploaded=0&downloaded=0\
+                    &left=1502576640&event=started&compact=1");
+    CURL *curl;
+    struct curl_slist *host = NULL;
+    path = path;
+    curl = curl_easy_init();
+    if (curl)
+    {
+        curl_easy_setopt(curl, CURLOPT_RESOLVE, host);
+        curl_easy_setopt(curl, CURLOPT_URL, n_path);
+        res = curl_easy_perform(curl);
+
+        /* always cleanup */
+        curl_easy_cleanup(curl);
+    }
+    free(n_path);
+    curl_slist_free_all(host);
+    return res;
 }
--- src/connect/connect.h
+++ src/connect/connect.h
@@ -1,5 +1,6 @@
 #ifndef CONNECT_H
 #define CONNECT_H
+#include <curl/curl.h>
 
 int dump_peers(const char *path);
 
